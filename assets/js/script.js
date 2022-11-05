c0af5431750f419f9f9092575c84ae2aa12fa75b
JaredIng87 <jaredingersoll14@gmail.com>
2022-11-05 18:04:36 -0400

    add answer buttons

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,27 @@
 var body = document.body;
 var startButton = document.querySelector(".start-button");
-//var timerElement = document.createElement("div");
 var headerElement = document.querySelector("header");
 var timerElement = document.querySelector(".timer");
 var quizQuestion = document.createElement("div");
+var answerButtons = document.createElement("div");
+var buttonOne = document.createElement('button');
+var buttonTwo = document.createElement('button');
+var buttonThree = document.createElement('button');
+var buttonFour = document.createElement('button');
 
+answerButtons.appendChild(buttonOne);
+answerButtons.appendChild(buttonTwo);
+answerButtons.appendChild(buttonThree);
+answerButtons.appendChild(buttonFour);
 
 quizQuestion.setAttribute('style', "margin-top:50px; text-align:center; font-size:25px;");
+answerButtons.setAttribute('style', "margin-top:50px; text-align:center; font-size:25px;");
 
-//body.appendChild(timerElement);
-body.appendChild(quizQuestion);
 
+buttonOne.textContent = 'one';
+buttonTwo.textContent = 'two';
+buttonThree.textContent = 'three';
+buttonFour.textContent = 'four';
 
 
 function countdown() {
@@ -31,6 +42,8 @@ var timeInterval = setInterval(function () {
 };
 
 startButton.addEventListener("click", function() {
+    body.appendChild(quizQuestion);
+    body.appendChild(answerButtons);
     quizQuestion.textContent = "getting there";
     countdown();
-})
+});
